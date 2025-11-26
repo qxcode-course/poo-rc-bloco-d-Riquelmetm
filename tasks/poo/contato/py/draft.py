eb2d54fecb05175fad6dff361265404ace637582
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-11-26 20:51:34 +0000

    cintatio

--- tasks/poo/contato/py/draft.py
+++ tasks/poo/contato/py/draft.py
@@ -1,7 +1,20 @@
 class Fone:
-    def __init__(self, nome: str, numero: int):
+    def __init__(self, nome: str, numero: str):
+        
         self.nome = nome
         self.fone = numero
 
     def __str__(self) -> str:
         return f"{self.nome}:{self.fone}"
+
+class Contato:
+    def __init__(self,contato:str):
+        self.nome_contato = contato
+        self.fones: list[Fone] = []
+        self.favorito =  False
+    
+    def adicionar(self, nome:str, numero:str):
+        n = list("0123456789()")
+        numero_listado = list(numero)
+        if numero_listado in  n:
+            
